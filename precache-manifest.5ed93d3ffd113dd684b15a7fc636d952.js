220a0fdf92b52648b48365146863c50dc18d51a0
ErlingFarland <ErlingFarland@bots.github.com>
2020-08-01 07:06:52 +0000

    updated GitHub Pages

---
+++ precache-manifest.5ed93d3ffd113dd684b15a7fc636d952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b97eb8bc1cf26b6ffac590b28d9dbc",
+    "url": "https://erlingfarland.github.io/xnh-voices/index.html"
+  },
+  {
+    "revision": "cfe4a5f62080d7628204",
+    "url": "https://erlingfarland.github.io/xnh-voices/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17f3b1a5eafcd55a0685",
+    "url": "https://erlingfarland.github.io/xnh-voices/static/js/2.5f225df1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://erlingfarland.github.io/xnh-voices/static/js/2.5f225df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe4a5f62080d7628204",
+    "url": "https://erlingfarland.github.io/xnh-voices/static/js/main.8ea164f8.chunk.js"
+  },
+  {
+    "revision": "0789a76ceeb059fc8921",
+    "url": "https://erlingfarland.github.io/xnh-voices/static/js/runtime-main.5ec20671.js"
+  }
+]);
